1c77f0d24e5a2b6052a1d025689c2a2a44d4458a
vikuuushnir <50868925+vikuuushnir@users.noreply.github.com>
2019-06-19 23:19:30 +0300

    Update CategoryKeeper.java

--- nearby-app/src/main/java/com/esri/android/nearbyplaces/data/CategoryKeeper.java
+++ nearby-app/src/main/java/com/esri/android/nearbyplaces/data/CategoryKeeper.java
@@ -8,28 +8,34 @@ import java.util.List;
 
 public class CategoryKeeper {
     private static CategoryKeeper instance = null;
-    private final ArrayList<FilterItem> categories = new ArrayList<>();
+    private final ArrayList<FilterItem> mCategories = new ArrayList<>();
+
     private CategoryKeeper(){
-        categories.add(new FilterItem("Бари", R.drawable.ic_local_bar_grey_48dp, true, R.drawable.ic_local_bar_black_24dp));
-        categories.add(new FilterItem("Готелі", R.drawable.ic_hotel_grey_24dp, true, R.drawable.ic_hotel_pink_24dp));
-        categories.add(new FilterItem("Відпочинок на природі", R.drawable.ic_nature_grey_24dp, true, R.drawable.ic_park_black_24dp));
-        categories.add(new FilterItem("Активний відпочинок", R.drawable.ic_fitness_center_grey_24dp, true, R.drawable.ic_fitness_center_pink_24dp));
+        // mCategories.add(new FilterItem("Bar", R.drawable.ic_local_bar_grey_48dp, true, R.drawable.ic_local_bar_blue_48dp));
+        mCategories.add(new FilterItem("Coffee Shop", R.drawable.ic_local_cafe_grey_48dp, true, R.drawable.ic_local_cafe_blue_48dp));
+        mCategories.add(new FilterItem("Food", R.drawable.ic_local_dining_grey_48dp, true, R.drawable.ic_local_dining_blue_48dp));
+        mCategories.add(new FilterItem("Hotel", R.drawable.ic_local_hotel_grey_48dp, true, R.drawable.ic_local_hotel_blue_48dp));
+        mCategories.add(new FilterItem("Park", R.drawable.ic_local_bar_grey_48dp, true, R.drawable.ic_local_cafe_blue_48dp));
+        mCategories.add(new FilterItem("Sports Center", R.drawable.ic_local_bar_grey_48dp, true, R.drawable.ic_local_cafe_blue_48dp));
     }
+
     public static CategoryKeeper getInstance(){
         if (CategoryKeeper.instance == null){
             CategoryKeeper.instance = new CategoryKeeper();
         }
         return CategoryKeeper.instance;
     }
+
     public final List<FilterItem> getCategories(){
-        return Collections.unmodifiableList(categories);
+        return Collections.unmodifiableList(mCategories);
     }
 
     public final List<String> getSelectedTypes(){
         final List<String> selectedTypes = new ArrayList<>();
-        for (final FilterItem item : categories){
+        for (final FilterItem item : mCategories){
             if (!item.getSelected()){
-
+                // Because places with food are sub-categorized by
+                // food type, add them to the filter list.
                 if (item.getTitle().equalsIgnoreCase("Food")){
                     selectedTypes.addAll(CategoryHelper.foodTypes);
                 }else {
