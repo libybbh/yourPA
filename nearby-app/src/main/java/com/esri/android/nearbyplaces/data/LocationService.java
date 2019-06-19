aab6b39b3ed803da510b4419c40d64f107c204d7
vikuuushnir <50868925+vikuuushnir@users.noreply.github.com>
2019-06-19 23:20:55 +0300

    Update LocationService.java

--- nearby-app/src/main/java/com/esri/android/nearbyplaces/data/LocationService.java
+++ nearby-app/src/main/java/com/esri/android/nearbyplaces/data/LocationService.java
@@ -1,26 +1,3 @@
-/* Copyright 2016 Esri
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * For additional information, contact:
- * Environmental Systems Research Institute, Inc.
- * Attn: Contracts Dept
- * 380 New York Street
- * Redlands, California, USA 92373
- *
- * email: contracts@esri.com
- *
- */
 package com.esri.android.nearbyplaces.data;
 
 import java.util.ArrayList;
@@ -121,10 +98,11 @@ public class LocationService implements PlacesServiceApi {
 
   private static void provisionCategories(@NonNull final GeocodeParameters parameters){
     final List<String> categories = parameters.getCategories();
+    categories.add("Food");
+    categories.add("Hotel");
+    categories.add("Coffee Shop");
     categories.add("Park");
     categories.add("Sports Center");
-    categories.add("Hotel");
-    categories.add("Bar or Pub");
   }
 
   private static void provisionOutputAttributes(@NonNull final GeocodeParameters parameters){
